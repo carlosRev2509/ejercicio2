ccccfaab8b2a551ef6e488ae49c20fdceea0a34f
Franz Gabriel <200908713@est.umss.edu>
2023-03-30 00:28:06 -0400

    creacion de suma.java

---
+++ Suma.java
@@ -0,0 +1,11 @@
+package programaAts;
+
+public class Suma {
+    int primero;
+    int segundo;
+    int resultado;
+    public suman(int primero, int segundo){
+        resultado = primero + segundo;
+        System.out.println(resultado);
+    }
+}
