0a66e35a762a050e20a86e369b47e454504acba0
Carlos Brian Revollo Villarroel <201900438@est.umss.edu>
2023-03-30 00:02:05 -0400

    Division Carlos Brian Revollo Villarroel

---
+++ Division.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author brian
+ */
+public class Division {
+    int a;
+    int b;
+    double resultado;
+    public Division(int a, int b){
+        resultado = (double)a / (double)b;
+        System.out.println(resultado);
+    }
+}
